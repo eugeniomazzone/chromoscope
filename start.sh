94d5ece2ece8143b7460beb6c00d3a07715ece58
eugenio <eugenio.mazzone@unito.it>
2025-01-15 16:22:32 +0100

    Start fix for config.json assembly

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo docker run --rm -dp 3000:3000 --name chromo  chromoscope:0.1
-sudo docker run --rm -dp 8080:8080 --name http http:0.1
+sudo docker run --rm -dp 3000:3000 --name chromo chromoscope:0.1
+sudo docker run --rm -dp 8080:8080 -i --name http -v $(pwd)/sampleData:/data http:0.1
+sudo docker exec -d http http-server /data/ --cors -c-1
 
 echo "Demo app at: http://127.0.0.1:3000/app/"
 echo "Demo data at: http://127.0.0.1:8080/"
-echo "Demo sample at: http://localhost:3000/app/?showSamples=true&external=http://127.0.0.1:8080/config.json"
+#echo "Demo sample at: http://localhost:3000/app/?showSamples=true&external=http://127.0.0.1:8080/config.json"
+#http://localhost:3000/app/?showSamples=true&external=http://127.0.0.1:8080/config.json
 echo "Demo sample at: http://127.0.0.1:3000/app/?showSamples=true&external=http://127.0.0.1:8080/config.json"
 
 echo "to stop containers run: sudo ./stop.sh"
