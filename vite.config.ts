d85001b65137fc305765cbc3227ad08e7c2c3767
eugenio <eugenio.mazzone@unito.it>
2025-02-11 11:11:15 +0100

    less sample for faster loading

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,20 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/app/',
+    base: '/chromocope/',
     build: { target: 'esnext' },
     optimizeDeps: {
         include: ['gosling.js']
+    },
+    server: {
+      port: 8080,
+      //host: "chromoscope",
+      // protocol: "ws",
+      hmr: {
+	host: "localhost",
+        //protocol: 'ws',
+        clientPort: 8080,
+      },
     },
     plugins: [reactRefresh()]
 });
